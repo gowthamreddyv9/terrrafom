591500c7a8b7bf3a143045b26b5ca6017cd214ec
Gowtham Reddy <gowthamreddy@Gowthams-MBP.attlocal.net>
2020-09-27 12:58:42 -0400

    adding the required code for creds

--- aws-getting-started/main.tf
+++ aws-getting-started/main.tf
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 provider "aws" {
    region = "AWS REGION"
    shared_credentials_file = "path/to/.aws/creds"
    profile = "AWS PROFILE"
-=======
-resource "aws_iam_instance_profile" "MyAWSResource" {
->>>>>>> 13eff116897fca9a5f4bb2fd569a05f6756ce8bb
 }
